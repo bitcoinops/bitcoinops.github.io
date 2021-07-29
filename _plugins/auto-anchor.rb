1eb1b0cd69547ec6c2cc39d3163e4c841dc3f98d
David A. Harding <dave@dtrt.org>
2021-07-29 09:44:22 -1000

    Plugins: auto-anchor.rb: add block filter version

--- _plugins/auto-anchor.rb
+++ _plugins/auto-anchor.rb
@@ -7,11 +7,8 @@
 # - [Summary][]: Details
 # - [Summary](URL): Details
 
-Jekyll::Hooks.register :documents, :pre_render do |post|
-  ## Don't process documents if YAML headers say: "auto_id: false" or
-  ## we're formatting for email
-  unless post.data["auto_id"] == false || ENV['JEKYLL_ENV'] == 'email'
-    post.content.gsub!(/^ *- .*/) do |string|
+def auto_anchor(content)
+    content.gsub!(/^ *- .*/) do |string|
       ## Find shortest match for **bold**, *italics*, or [markdown][links]
       title = string.match(/\*\*.*?\*\*|\*.*?\*|\[.*?\][(\[]/).to_s
 
@@ -27,5 +24,31 @@ Jekyll::Hooks.register :documents, :pre_render do |post|
         string.sub!(/-/, id_prefix)
       end
     end
+end
+
+## Run automatically on all documents
+Jekyll::Hooks.register :documents, :pre_render do |post|
+  ## Don't process documents if YAML headers say: "auto_id: false" or
+  ## we're formatting for email
+  unless post.data["auto_id"] == false || ENV['JEKYLL_ENV'] == 'email'
+    auto_anchor(post.content)
   end
 end
+
+## Block filter that provides {% auto_anchor %}{% endauto_anchor %} for
+## use on {% include %} files
+module Jekyll
+  class RenderAutoAnchor < Liquid::Block
+
+    def render(context)
+      text = super
+      text = auto_anchor(text)
+      text = Liquid::Template.parse(text)
+      text.render(@context)
+    end
+
+  end
+end
+
+Liquid::Template.register_tag('auto_anchor', Jekyll::RenderAutoAnchor)
+
